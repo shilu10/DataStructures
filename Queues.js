46bd5c6b5b126ee382c905f02f2e7688a01b1223
shilu10 <89338633+shilu10@users.noreply.github.com>
2021-08-22 17:06:57 +0530

    Update Queues.js

--- Queues.js
+++ Queues.js
@@ -5,30 +5,37 @@ class NewNode{
   }
 }
 class Queue{
-  constructor(value){
-    this.head={
-     value:value,
-     next:null
+  constructor(){
+    this.first={
+    value:null,
+    next:null
     };
-    this.tail=this.head;
+    this.last=this.first;
     this.length=0;
   }
 
   enqueue(value){
-    let currentnode=this.head;
-    let newnode=new NewNode(value)
-    this.head=newnode
-    this.head.next=currentnode;
-    this.length++;
+   let newnode=new NewNode(value)
+   if (this.length===0){
+     this.first.value=value;
+
+     this.length++
+     return this
+   }
+   this.last.next=newnode;
+   this.last=newnode;
+   this.length++;
+   return this;
   }
   dequeue(){
-    let currentnode=this.head;
-    this.head=currentnode.next;
+    let currentnode=this.first;
+    this.first=currentnode.next;
+    this.length--;
     return this
   }
     printlist(){
     let list=[]
-    let currentnode=this.head
+    let currentnode=this.first
     while (currentnode){
       
       list.push(currentnode.value)
@@ -37,14 +44,29 @@ class Queue{
     }
     return list;
   }
+  peek(){
+    if (this.length>0){
+    return this.first.value
+    }
+    return "None"
+  }
+  isempty(){
+    if (this.length===0){
+      return "True"
+    }
+    return "False"
+  }
 }
 
 
-queue=new Queue(12)
-//queue.enqueue(100)
-//queue.enqueue(101)
-//queue.enqueue(1010)
-//queue.dequeue()
-//queue.dequeue()
-//console.log(queue.printlist())
+queue=new Queue()
+queue.enqueue(100)
+queue.enqueue(101)
+queue.enqueue(1010)
+queue.dequeue()
+queue.dequeue()
+queue.dequeue()
+console.log(queue.isempty())
+console.log(queue.peek())
 //console.log(queue)
+//console.log(queue.printlist())
